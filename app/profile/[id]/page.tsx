57abd98c79ab42f01305be11952fcdef3d647b39
vijay-singh07 <vs8262836@gmail.com>
2024-03-06 03:13:04 +0530

    fix/profile page update with new component

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -6,6 +6,7 @@ import ProfileContent from './components/ProfileContent'
 import useGetUserDetails from '@/hooks/useGetUserDetails'
 import getSongsByUserId from '@/actions/getSongsByUserId';
 import { useUser } from '@/hooks/useUser'
+import HeaderProfile from '@/components/HeaderProfile'
 
 interface ProfileProps {
     params: {
@@ -32,33 +33,11 @@ const page:React.FC<ProfileProps> = async({params}) => {
     w-full
     overflow-hidden
     overflow-y-auto">
-        <Header className={""}>
-          <div className="mb-2">
-            <h1 className=" text-white
-            text-3xl
-            font-semibold">
-              Welcome Back
-            </h1>
-            <div className="
-            grid
-            grid-cols-1
-            sm:grid-cols-2
-            xl:grid-cols-3
-            2xl:grid-cols-4
-            gap-3
-            mt-4">
-              <ListItem
-              image= "/images/liked.png"
-              name="Liked Songs"
-              href="liked"
-              />
-            </div>
-          </div>
-        </Header>
+        <HeaderProfile className={""}/>
         <div className="mt-2 mb-7 px-6">
           <div className="flex justify-between items-center">
             <h1 className="text-white text-2xl font-semibold">
-              Newest Songs
+              Your Profile
             </h1>
           </div>
           <ProfileContent userId={userId} songs={songs}/>
